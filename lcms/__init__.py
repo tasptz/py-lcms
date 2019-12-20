a30cec002e4b5d8b72d681963ee67331274a90fc
Thomas Pönitz <tasptz@gmail.com>
2019-12-20 10:41:24 +0100

    handle different data types

--- lcms/__init__.py
+++ lcms/__init__.py
@@ -4,8 +4,6 @@ from . import pylcms
 __version__ = '0.1.0'
 
 def apply_profile(input_image, input_profile, output_profile='srgb', in_place=False):
-    if input_image.dtype != np.uint16:
-        raise TypeError()
     if in_place:
         if not input_image.flags['C_CONTIGUOUS']:
             raise RuntimeError('Input not contiguous')
--- tests/test_apply_profile.py
+++ tests/test_apply_profile.py
@@ -1,8 +1,22 @@
 import pytest
 
-def test_identity():
+def test_identity_8():
     import numpy as np
-    img = np.zeros((256, 256, 3), dtype=np.uint16)
+    img = np.random.randint(0, 2**8 - 1, (256, 256, 3), np.uint8)
     from lcms import apply_profile
     img_t = apply_profile(img, 'srgb')
     assert np.array_equal(img, img_t)
+
+def test_identity_16():
+    import numpy as np
+    img = np.random.randint(0, 2**16 - 1, (256, 256, 3), np.uint16)
+    from lcms import apply_profile
+    img_t = apply_profile(img, 'srgb')
+    assert np.array_equal(img, img_t)
+
+def test_identity_flt():
+    import numpy as np
+    img = np.random.rand(4, 4, 3).astype(np.float32)
+    from lcms import apply_profile
+    img_t = apply_profile(img, 'srgb')
+    assert np.allclose(img, img_t)
